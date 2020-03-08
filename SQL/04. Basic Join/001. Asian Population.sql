107492d6c92c0de259be812e3ccd41612eb2e88e
Andrei Micuda <andrei.micuda@my.fmi.unibuc.ro>
2020-03-08 19:14:45 +0200

    Create 001. Asian Population.sql

---
+++ SQL/04. Basic Join/001. Asian Population.sql
@@ -0,0 +1,7 @@
+- Problem: https://www.hackerrank.com/challenges/asian-population
+
+SELECT SUM(CITY.POPULATION)
+FROM CITY, COUNTRY
+WHERE
+    CITY.COUNTRYCODE = COUNTRY.CODE
+    AND COUNTRY.CONTINENT = 'Asia';
